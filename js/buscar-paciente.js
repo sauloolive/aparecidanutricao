192efa79422686d74d9ed775359546be75446edb
Saulo Oliver <63970553+sauloolive@users.noreply.github.com>
2023-07-05 14:30:53 -0300

    Add files via upload

---
+++ js/buscar-paciente.js
@@ -0,0 +1,22 @@
+var botaoAdicionar = document.querySelector("#buscar-pacientes");
+
+botaoAdicionar.addEventListener("click", function(){
+    console.log("Buscando pacientes...");
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://raw.githubusercontent.com/loresgarcia/Pacientes-API/master/pacientes.json");
+
+    xhr.addEventListener("load", function(){
+        var resposta = xhr.responseText;
+        var pacientes = JSON.parse(resposta);
+        
+        pacientes.forEach( function(paciente) {
+            adicionaPacienteNaTabela(paciente);
+        });
+        
+    });
+    
+    xhr.send();
+
+});
